f7b5fd299779670a6debd12a7f4ce11aef1b836d
Shafina Ardelia <130486863+Meadowiee@users.noreply.github.com>
2023-09-22 23:51:13 +0800

    Create Praktikum03.java
    -	Buatlah program yang menampilkan hasil nilai rata-rata dari empat variable jika diketahui Nilai 1 = 17, Nilai 2 = 22, Nilai 3 = 31, dan Nilai 4 = 43 dengan hasil yang tidak dibulatkan.

---
+++ 02. Operators in Java/Praktikum03.java
@@ -0,0 +1,20 @@
+public class Praktikum03.java { 
+    public static void main(String[] args) {
+        
+        double nilai1, nilai2, nilai3, nilai4, rataNilai; 
+      
+        // Value   
+        nilai1 = 17;
+        nilai2 = 22;
+        nilai3 = 31;
+        nilai4 = 43;
+      
+        // Process
+        rataNilai = nilai1 + nilai2 + nilai3 + nilai4;
+        rataNilai = rataNilai/4; 
+
+        //Output
+        System.out.println("Rata-rata = " + rataNilai); 
+    } 
+     
+}
